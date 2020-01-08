2dfd0504d7727dd16731ab2fff3a1663211ad20f
Michelangelo <Michelangelo@DESKTOP-C01FU0U>
2020-01-08 16:10:08 +0100

    Add project files.

---
+++ Compito_Vacanze/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Compito_Vacanze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            Console.WriteLine(a);
+            if (a == 214783647)
+            {
+                Console.WriteLine(a + 1);
+            }
+            int b = 128;
+            int c = -b;
+            Console.WriteLine(Convert.ToString(b, 2));
+            Console.WriteLine(Convert.ToString(c, 2));
+            int value = 31;
+            int somma = value;
+            value = ~value + 1; 
+            somma = somma + value;
+            Console.WriteLine($"{value} e {somma}");
+            long number = 55555;
+            Console.WriteLine(Convert.ToString(number, 2));
+            short number1 = (short)number;
+            Console.WriteLine(number1);
+        }
+    }
+    }
+}
